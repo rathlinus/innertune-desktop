d6c98b72e842c655d4e31688133b7b30d65f05e1
Linus Rath <139418639+rathlinus@users.noreply.github.com>
2026-08-21 09:52:39 +0200

    Remove scratch probe files

--- frontend/_probe_main.cjs
+++
@@ -1,5 +0,0 @@
-const { app } = require("electron");
-app.on("ready", async () => {
-  try { require("./_probe_entry.cjs"); } catch (e) { console.log("ERR", e); }
-  setTimeout(() => app.exit(0), 25000);
-});
